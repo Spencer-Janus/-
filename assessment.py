186ed0e9a5906a10b965731590b226917bae6049
慕思oc <75158822+Spencer-Janus@users.noreply.github.com>
2020-12-20 15:09:32 +0800

    Delete assessment.py

--- assessment.py
+++
@@ -1,9 +0,0 @@
-def assessment(grade):
-    num=0
-    f=open("grades.txt","r")
-    list_=f.readlines()
-    list_grade=[float(i.replace('\n','')) for i in list_]
-    for i in list_grade:
-        if grade>i:
-            num=num+1
-    return round(num/len(list_grade)*100,1)
